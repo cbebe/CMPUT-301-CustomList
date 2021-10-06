20760fea5b14b058e2e6a66365a271783aab8743
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-10-05 18:49:18 -0600

    finish demo

--- app/src/main/java/com/example/simpleparadox/listycity/MainActivity.java
+++ app/src/main/java/com/example/simpleparadox/listycity/MainActivity.java
@@ -1,56 +1,84 @@
 package com.example.simpleparadox.listycity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "Sample";
+    FirebaseFirestore db;
+    private ArrayAdapter<City> cityAdapter;
+    private ArrayList<City> cityDataList;
+    private EditText addCityEditText;
+    private EditText addProvinceEditText;
 
-    // Declare the variables so that you will be able to reference it later.
-    ListView cityList;
-    ArrayAdapter<City> cityAdapter;
-    ArrayList<City> cityDataList;
-
-    CustomList customList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cityList = findViewById(R.id.city_list);
-
-        String []cities ={"Edmonton", "Vancouver", "Toronto", "Hamilton", "Denver", "Los Angeles"};
-        String []provinces = {"AB", "BC", "ON", "ON", "CO", "CA"};
+        // Declare the variables so that you will be able to reference it later.
+        final ListView cityList = findViewById(R.id.city_list);
+        final Button addCityButton = findViewById(R.id.add_city_button);
+        addCityEditText = findViewById(R.id.add_city_field);
+        addProvinceEditText = findViewById(R.id.add_province_edit_text);
 
 
         cityDataList = new ArrayList<>();
-
-        for(int i=0;i<cities.length;i++){
-            cityDataList.add((new City(cities[i], provinces[i])));
-        }
-
         cityAdapter = new CustomList(this, cityDataList);
-
         cityList.setAdapter(cityAdapter);
 
-//        dataList = new ArrayList<>();
-//        dataList.addAll(Arrays.asList(cities));
-//
-//        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
-//
-//        cityList.setAdapter(cityAdapter);
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("Cities");
 
+        addCityButton.setOnClickListener(view -> {
+            final String cityName = addCityEditText.getText().toString();
+            final String provinceName = addProvinceEditText.getText().toString();
+            HashMap<String, String> data = new HashMap<>();
 
+            if (cityName.length() > 0 && provinceName.length() > 0) {
+                data.put("Province Name", provinceName);
+                collectionReference
+                        .document(cityName)
+                        .set(data)
+                        .addOnSuccessListener(aVoid -> {
+                            // These are a method which gets executed when the task is succeeded
+                            Log.d(TAG, "Data has been added successfully!");
+                        })
+                        .addOnFailureListener(e -> {
+                            // These are a method which gets executed if there’s any problem
+                            Log.d(TAG, "Data could not be added!" + e.toString());
+                        });
+                addCityEditText.setText("");
+                addProvinceEditText.setText("");
 
+            }
+        });
+
+        collectionReference.addSnapshotListener((queryDocumentSnapshots, error) -> {
+            cityDataList.clear();
+            assert queryDocumentSnapshots != null;
+            for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+            {
+                Log.d(TAG, String.valueOf(doc.getData().get("Province Name")));
+                String city = doc.getId();
+                String province = (String) doc.getData().get("Province Name");
+                cityDataList.add(new City(city, province)); // Adding the cities and provinces from FireStore
+            }
+            cityAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
+        });
     }
-
-
 }
