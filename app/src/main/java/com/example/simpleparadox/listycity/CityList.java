3162f96b21fa4c1372daa54658e0a43e520735a1
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-10-16 15:02:18 -0600

    Finish setting up CityList for unit tests
    I added methods that will need to be tested for the participation
    exercise.

--- app/src/main/java/com/example/simpleparadox/listycity/CityList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CityList.java
@@ -32,4 +32,32 @@ public class CityList {
         Collections.sort(list);
         return list;
     }
+
+    /**
+     * Checks if a city is present in the list.
+     * If it does then remove it from the list, if not then throw an exception
+     *
+     * @param city This is a candidate city to delete
+     * @throws Exception if city is not present in the list
+     */
+    public void delete(City city) throws Exception {
+//        this.cities.remove(city)
+    }
+
+    /**
+     * Find given city in the list
+     *
+     * @param city This is a candidate city to find
+     * @return whether or not it belongs in the list
+     */
+    public boolean hasCity(City city) {
+        return false;
+    }
+
+    /**
+     * @return how many cities are in the list
+     */
+    public int countCities() {
+        return -1;
+    }
 }
--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -40,4 +40,13 @@ class CityListTest {
         assertEquals(0, city.compareTo(cityList.getCities().get(0)));
         assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
     }
+
+    @Test
+    void testHasCity(){
+        CityList cityList = mockCityList();
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.add(city);
+        assertTrue(cityList.hasCity(city));
+    }
 }
