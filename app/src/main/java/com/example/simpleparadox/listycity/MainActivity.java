ffcecf6e4ff6d1142b16d982df57bb7269bad233
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-10-29 19:15:19 -0600

    Oop

--- app/src/main/java/com/example/simpleparadox/listycity/MainActivity.java
+++ app/src/main/java/com/example/simpleparadox/listycity/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     public static final String CITY_EXTRA = "com.example.simpleparadox.listycity.CITY_EXTRA";
